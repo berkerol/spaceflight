32577985fabfd0a6e6ad6fb7249bff11f6dd5705
Berk Erol <berk.erol@boun.edu.tr>
2019-06-02 14:21:45 +0300

    Cleanup

--- spaceflight.js
+++ spaceflight.js
@@ -102,11 +102,7 @@ function removeStars (frames) {
       s.x += (s.x - mouse.x) / canvas.width * s.speed * frames;
       s.y += (s.y - mouse.y) / canvas.height * s.speed * frames;
       s.radius += s.depth * frames;
-      if (s.alpha + s.alphaIncrease * frames <= 1.0) {
-        s.alpha += s.alphaIncrease * frames;
-      } else {
-        s.alpha = 1.0;
-      }
+      s.alpha = Math.min(s.alpha + s.alphaIncrease * frames, 1.0);
     }
   }
 }
